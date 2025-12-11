17c6e94545805ce1e5cf2450963a59a69fbd49dc
DavidZarfati <zarda2001@gmail.com>
2025-12-11 18:30:03 +0100

    feat:modifica pagina chi siamo

--- src/Pages/Chi-siamo.jsx
+++ src/Pages/Chi-siamo.jsx
@@ -1,9 +1,34 @@
 export default function Chisiamo() {
     return (
         <>
-            <h1>About US</h1>
-            <h2>qui tutto quello che devi sapere su di noi</h2>
-            <h4>qui in piccolo alcuni nostri contatti</h4>
+            <div className="homePage">
+                <h2>Chi siamo?</h2>
+                <h3>Perche Comprare Da Noi, Vuol Dire Comprare Da Vere Persone</h3>
+                <p>Qui Trovi Cosa Ci Distingue Dalle Altre Aziende, La nostra Storia, le Persone che ci hanno portato fino a questo punto</p>
+                <div className="col row-cols-3 d-flex flex-wrap justify-content-center">
+
+                    <div className="card">
+                        <img className="card-img-top" src="..." alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Foto Genitore</h5>
+                            <p className="card-text">Piccola Caratterizzazione della foto</p>
+                        </div>
+                    </div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad unde aliquid doloribus animi, sit temporibus laborum, nam soluta possimus in, accusantium perferendis repellendus natus quos cumque eligendi impedit consequatur quam.
+                        Quia unde fuga nobis, quaerat ipsam maxime porro error, vero corporis temporibus aliquam in? Distinctio tempora consequatur deserunt aperiam fugit officiis blanditiis architecto debitis ab magni, ad totam beatae earum?
+                        A assumenda quia eius deserunt perferendis nihil. Tenetur quidem dolorem at distinctio corrupti laudantium velit, rem, eos aperiam quis labore laborum consectetur totam et est nesciunt pariatur nam dolores expedita.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates sunt facilis consequuntur porro error neque officia blanditiis possimus atque quibusdam magnam voluptatum, eos deleniti eaque, odio sint maiores rem vel!
+                        Voluptatum dolorum voluptatem repellat possimus rem, illum doloremque fugit odit necessitatibus ipsam aut sed obcaecati enim sequi quod totam velit repellendus iure vero incidunt quisquam nemo laudantium. Libero, labore soluta?
+                        Maxime alias architecto esse quo libero. Illo vel explicabo accusantium sequi nostrum laudantium, modi quae possimus, deleniti hic eos libero at autem. Molestias dolores provident neque similique eum obcaecati praesentium.</p>
+                    <div className="card">
+                        <img className="card-img-top" src="..." alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Ruolo Nell'azienda</h5>
+                            <p className="card-text">Piccola Caratterizzazione della foto</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
