e2fcab8cfc1912695cd1f28fff3a7d651c3d09e2
DavidZarfati <zarda2001@gmail.com>
2025-12-11 15:07:40 +0100

    feat:fine setup iniziale e correzione di bootstrap icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap/dist/css/bootstrap.css"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
